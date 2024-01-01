f787b0d6ea414977c7af8a57014ddaf4479b9435
Tasuku Suzuki <tasuku.suzuki@slint.dev>
2024-01-01 12:52:30 +0900

    WIP: add dial support to m5stack dial

--- examples/mcu-board-support/esp32_m5stack_dial.rs
+++ examples/mcu-board-support/esp32_m5stack_dial.rs
@@ -15,7 +15,8 @@ use esp32s3_hal::{
     prelude::*,
     spi::{Spi, SpiMode},
     systimer::SystemTimer,
-    Delay, IO,
+    timer::TimerGroup,
+    Delay, Rtc, IO,
 };
 use esp_alloc::EspHeap;
 use esp_backtrace as _;
@@ -23,8 +24,32 @@ use gc9a01::GC9A01;
 use slint::platform::WindowEvent;
 pub use xtensa_lx_rt::entry;
 
+#[cfg(feature = "dial")]
+use critical_section::Mutex;
+#[cfg(feature = "dial")]
+use esp32s3_hal::{
+    interrupt,
+    peripherals,
+    pcnt::{
+        PCNT,
+        channel::{self, PcntSource},
+        unit::{self, Unit},
+    },
+};
+#[cfg(feature = "dial")]
+use core::{
+    cmp::min,
+    sync::atomic::{AtomicI32, Ordering},
+};
+
+
 #[global_allocator]
 static ALLOCATOR: EspHeap = EspHeap::empty();
+
+#[cfg(feature = "dial")]
+static UNIT0: Mutex<RefCell<Option<Unit>>> = Mutex::new(RefCell::new(None));
+#[cfg(feature = "dial")]
+static VALUE: AtomicI32 = AtomicI32::new(0);
 
 pub fn init() {
     const HEAP_SIZE: usize = 250 * 1024;
@@ -60,6 +85,18 @@ impl slint::platform::Platform for EspBackend {
         let peripherals = Peripherals::take();
         let mut system = peripherals.SYSTEM.split();
         let clocks = ClockControl::configure(system.clock_control, CpuClock::Clock240MHz).freeze();
+
+        let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+        let timer_group0 =
+            TimerGroup::new(peripherals.TIMG0, &clocks, &mut system.peripheral_clock_control);
+        let mut wdt0 = timer_group0.wdt;
+        let timer_group1 =
+            TimerGroup::new(peripherals.TIMG1, &clocks, &mut system.peripheral_clock_control);
+        let mut wdt1 = timer_group1.wdt;
+
+        rtc.rwdt.disable();
+        wdt0.disable();
+        wdt1.disable();
 
         let mut delay = Delay::new(&clocks);
         let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
@@ -105,6 +142,60 @@ impl slint::platform::Platform for EspBackend {
         let mut backlight = io.pins.gpio9.into_push_pull_output();
         backlight.set_high().unwrap();
 
+        #[cfg(feature = "dial")]
+        {
+            let mut mtdo = io.pins.gpio40.into_pull_up_input();
+            let mut mtdi = io.pins.gpio41.into_pull_up_input();
+            let pcnt = PCNT::new(peripherals.PCNT, &mut system.peripheral_clock_control);
+            let mut u0: unit::Unit = pcnt.get_unit(unit::Number::Unit1);
+            u0.configure(unit::Config {
+                low_limit: -100,
+                high_limit: 100,
+                filter: Some(min(10u16 * 80, 1023u16)),
+                ..Default::default()
+            })
+            .unwrap();
+            let mut ch0 = u0.get_channel(channel::Number::Channel0);
+            ch0.configure(
+                PcntSource::from_pin(&mut mtdi),
+                PcntSource::from_pin(&mut mtdo),
+                channel::Config {
+                    lctrl_mode: channel::CtrlMode::Reverse,
+                    hctrl_mode: channel::CtrlMode::Keep,
+                    pos_edge: channel::EdgeMode::Decrement,
+                    neg_edge: channel::EdgeMode::Increment,
+                    invert_ctrl: false,
+                    invert_sig: false,
+                },
+            );
+            let mut ch1 = u0.get_channel(channel::Number::Channel1);
+            ch1.configure(
+                PcntSource::from_pin(&mut mtdo),
+                PcntSource::from_pin(&mut mtdi),
+                channel::Config {
+                    lctrl_mode: channel::CtrlMode::Reverse,
+                    hctrl_mode: channel::CtrlMode::Keep,
+                    pos_edge: channel::EdgeMode::Increment,
+                    neg_edge: channel::EdgeMode::Decrement,
+                    invert_ctrl: false,
+                    invert_sig: false,
+                },
+            );
+            u0.events(unit::Events {
+                low_limit: true,
+                high_limit: true,
+                thresh0: false,
+                thresh1: false,
+                zero: false,
+            });
+            u0.listen();
+            u0.resume();
+    
+            critical_section::with(|cs| UNIT0.borrow_ref_mut(cs).replace(u0));
+    
+            interrupt::enable(peripherals::Interrupt::PCNT, interrupt::Priority::Priority2).unwrap();
+        }
+        
         let size = slint::PhysicalSize::new(240, 240);
 
         self.window.borrow().as_ref().unwrap().set_size(size);
@@ -116,6 +207,8 @@ impl slint::platform::Platform for EspBackend {
 
         let mut last_touch = None;
         let button = slint::platform::PointerEventButton::Left;
+        #[cfg(feature = "dial")]
+        let mut last_dial_value = 0;
 
         loop {
             slint::platform::update_timers_and_animations();
@@ -146,6 +239,21 @@ impl slint::platform::Platform for EspBackend {
                     window.dispatch_event(event);
                 }
 
+                #[cfg(feature = "dial")]
+                critical_section::with(|cs| {
+                    let mut u0 = UNIT0.borrow_ref_mut(cs);
+                    let u0 = u0.as_mut().unwrap();
+                    let value: i32 = u0.get_value() as i32 + VALUE.load(Ordering::SeqCst);
+                    if (last_dial_value - value).abs() > 3  {
+                        window.dispatch_event(WindowEvent::PointerScrolled {
+                            position: last_touch.unwrap_or(slint::LogicalPosition::new(0f32, 0f32)),
+                            delta_x: 0f32,
+                            delta_y: (last_dial_value - value) as f32,
+                        });
+                        last_dial_value = value;
+                    }
+                });
+        
                 window.draw_if_needed(|renderer| {
                     renderer.render_by_line(&mut buffer_provider);
                 });
@@ -160,6 +268,24 @@ impl slint::platform::Platform for EspBackend {
     fn debug_log(&self, arguments: core::fmt::Arguments) {
         esp_println::println!("{}", arguments);
     }
+}
+
+#[cfg(feature = "dial")]
+#[interrupt]
+fn PCNT() {
+    critical_section::with(|cs| {
+        let mut u0 = UNIT0.borrow_ref_mut(cs);
+        let u0 = u0.as_mut().unwrap();
+        if u0.interrupt_set() {
+            let events = u0.get_events();
+            if events.high_limit {
+                VALUE.fetch_add(100, Ordering::SeqCst);
+            } else if events.low_limit {
+                VALUE.fetch_add(-100, Ordering::SeqCst);
+            }
+            u0.reset_interrupt();
+        }
+    });
 }
 
 struct DrawBuffer<'a, Display> {
